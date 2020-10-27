81be87736edcf68f7eca3f390417cfddf1af1154
yuan qihang <qihang.yuan@thoughtworks.com>
2020-10-28 01:17:01 +0800

    完成分组功能

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/UserController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/UserController.java
@@ -6,13 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
+@CrossOrigin
 @RestController
 public class UserController {
     @Autowired
@@ -22,6 +21,12 @@ public class UserController {
     @ResponseStatus(HttpStatus.OK)
     public List<User> getUsers() {
         return userService.getUsers();
+    }
+
+    @GetMapping(path = "/users/split")
+    @ResponseStatus(HttpStatus.OK)
+    public Map<String,List<User>> getUsersRandomList() {
+        return userService.getUsersRandomList();
     }
 
 }
--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/UserService.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/UserService.java
@@ -1,11 +1,13 @@
 package com.thoughtworks.capability.gtb.entrancequiz.service;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import com.thoughtworks.capability.gtb.entrancequiz.entity.User;
 import com.thoughtworks.capability.gtb.entrancequiz.repository.UserRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import javax.jws.soap.SOAPBinding;
+import java.util.*;
 
 @Service
 public class UserService {
@@ -14,5 +16,49 @@ public class UserService {
 
     public List<User> getUsers() {
         return userRespository.findAll();
+    }
+
+    public Map<String, List<User>> getUsersRandomList() {
+        Random random = new Random();
+        List<User> users = userRespository.findAll();
+        HashMap<String, List<User>> map = new HashMap<>();
+        LinkedList<User> ls1 = new LinkedList<>();
+        LinkedList<User> ls2 = new LinkedList<>();
+        LinkedList<User> ls3 = new LinkedList<>();
+        LinkedList<User> ls4 = new LinkedList<>();
+        LinkedList<User> ls5 = new LinkedList<>();
+        LinkedList<User> ls6 = new LinkedList<>();
+        int cicle = users.size()/6;
+        for (int i = 0; i < cicle; i++) {
+            ls1.add(users.remove(random.nextInt(users.size())));
+            ls2.add(users.remove(random.nextInt(users.size())));
+            ls3.add(users.remove(random.nextInt(users.size())));
+            ls4.add(users.remove(random.nextInt(users.size())));
+            ls5.add(users.remove(random.nextInt(users.size())));
+            ls6.add(users.remove(random.nextInt(users.size())));
+        }
+
+        if(users.size()>0){
+            ls1.add(users.remove(random.nextInt(users.size())));
+        }
+        if(users.size()>0){
+            ls2.add(users.remove(random.nextInt(users.size())));
+        }
+        if(users.size()>0){
+            ls3.add(users.remove(random.nextInt(users.size())));
+        }
+        if(users.size()>0){
+            ls4.add(users.remove(random.nextInt(users.size())));
+        }
+        if(users.size()>0){
+            ls5.add(users.remove(random.nextInt(users.size())));
+        }
+        map.put("1 组",ls1);
+        map.put("2 组",ls2);
+        map.put("3 组",ls3);
+        map.put("4 组",ls4);
+        map.put("5 组",ls5);
+        map.put("6 组",ls6);
+        return map;
     }
 }
